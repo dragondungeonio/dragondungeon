54510556fdfe8b0e54401c120eb6c8e07d8da55e
keithagroves <keith.groves@jointheleague.org>
2022-03-08 20:37:26 -0800

    add gamesize to config (#13)

--- common/GameState.ts
+++ common/GameState.ts
@@ -1,73 +1,66 @@
-import {
-	Schema,
-	type,
-	MapSchema,
-	ArraySchema
-} from '@colyseus/schema';
+import { Schema, type, MapSchema, ArraySchema } from '@colyseus/schema'
 
-import { Player } from './Player';
-import { Coin } from './Coin';
-import { CoinJar } from './CoinJar';
-import { v4 } from "uuid";
-import { BorderFence } from './BorderFence';
-import { Countdown } from './Countdown';
-import { Bat } from './Bat';
-import { Skull } from './Skull';
-import {Wall} from './Wall';
+import { Player } from './Player'
+import { Coin } from './Coin'
+import { CoinJar } from './CoinJar'
+import { v4 } from 'uuid'
+import { BorderFence } from './BorderFence'
+import { Countdown } from './Countdown'
+import { Bat } from './Bat'
+import { Skull } from './Skull'
+import { Wall } from './Wall'
+import gameConfig from '../config/dragondungeon.config'
 
 export class GameState extends Schema {
-	@type("boolean")
-	first: boolean = false;
+  @type('boolean')
+  first: boolean = false
 
-	@type({
-		map: Player
-	})
-	players = new MapSchema < Player > ();
+  @type({
+    map: Player,
+  })
+  players = new MapSchema<Player>()
 
-	@type({map: Coin})
-	coins = new MapSchema < Coin > ();
+  @type({ map: Coin })
+  coins = new MapSchema<Coin>()
 
-	@type({map: BorderFence})
-	fences = new MapSchema < BorderFence > ();
+  @type({ map: BorderFence })
+  fences = new MapSchema<BorderFence>()
 
-	@type(Countdown)
-	countdown = new Countdown(3, 0);
+  @type(Countdown)
+  countdown = new Countdown(3, 0)
 
-	@type({map: Bat})
-	bats = new MapSchema <Bat>();
+  @type({ map: Bat })
+  bats = new MapSchema<Bat>()
 
-	@type({map: Skull})
-	skulls = new MapSchema <Skull>();
+  @type({ map: Skull })
+  skulls = new MapSchema<Skull>()
 
-	@type("boolean")
-	debugOn: boolean = false;
+  @type('boolean')
+  debugOn: boolean = false
 
-	@type("boolean")
-	gameOver: boolean = false;
+  @type('boolean')
+  gameOver: boolean = false
 
-	@type({map: Wall})
-	walls = new MapSchema<Wall>();
+  @type({ map: Wall })
+  walls = new MapSchema<Wall>()
 
+  @type('number')
+  batRot: number = 0
 
-	@type("number")
-	batRot: number = 0;
+  @type('number')
+  gamewidth: number = gameConfig.gameSize
 
-	@type("number")
-	gamewidth: number = 3000;
+  @type('number')
+  gameheight: number = gameConfig.gameSize
 
-	@type("number")
-	gameheight: number = 3000;
+  @type(CoinJar)
+  coinJar = new CoinJar(this.gamewidth / 2, this.gameheight / 2)
 
+  //"FFA" or "coinCapture"
+  @type('string')
+  gamemode: string = 'FFA'
 
-	@type(CoinJar)
-	coinJar = new CoinJar(this.gamewidth/2, this.gameheight/2);
-
-	//"FFA" or "coinCapture"
-	@type("string")
-	gamemode: string = "FFA"
-	
-	constructor() {
-		super();
-	}
+  constructor() {
+    super()
+  }
 }
-
